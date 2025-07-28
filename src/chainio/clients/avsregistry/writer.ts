8a6e6871da606e14b97b7760bbfc616b617eb929
sadegh <sadeghte@gmail.com>
2025-07-28 18:17:42 +0330

    bug fix & add logs

--- src/chainio/clients/avsregistry/writer.ts
+++ src/chainio/clients/avsregistry/writer.ts
@@ -6,8 +6,13 @@ import { Web3, Contract, Address, TransactionReceipt } from "web3";
 import { ethers } from 'ethers'
 import { G1Point, KeyPair, Signature } from "../../../crypto/bls/attestation";
 import * as chainIoUtils from '../../utils'
+import { sendContractCall } from '../../utils.js'
 import * as ABIs from '../../../contracts/ABIs'
-import { LocalAccount, OperatorDirectedRewardsSubmissions, OperatorSetParams, QuorumNum, RewardsSubmission, StrategyParams, Uint16, Uint256, Uint32, Uint96 } from "../../../types/general";
+import { 
+    LocalAccount, OperatorDirectedRewardsSubmission, 
+    OperatorSetParams, QuorumNum, RewardsSubmission, 
+    StrategyParams, Uint16, Uint256, Uint32, Uint96 
+} from "../../../types/general";
 import { signRawData } from "../../../utils/helpers";
 
 const DEFAULT_QUERY_BLOCK_RANGE = 10_000;
@@ -57,68 +62,86 @@ export class AvsRegistryWriter {
         this.transactor = new chainIoUtils.Transactor(pkWallet, ethHttpClient);
     }
 
-    async sendTransaction(contract: Contract<any>, method: string, params: any[]): Promise<TransactionReceipt> {
-        return this.transactor.send(contract, method, params)
-    }
+    // async sendTransaction(contract: Contract<any>, method: string, params: any[]): Promise<TransactionReceipt> {
+    //     return this.transactor.send(contract, method, params)
+    // }
 
     async updateStakesOfEntireOperatorSetForQuorums(
         operatorsPerQuorum: string[][],
         quorumNumbers: QuorumNum[]
     ): Promise<TransactionReceipt> {
         const quorumBytes = chainIoUtils.numsToBytes(quorumNumbers.map(Number));
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'updateOperatorsForQuorum',
-            [operatorsPerQuorum, quorumBytes]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'updateOperatorsForQuorum',
+            params: [operatorsPerQuorum, quorumBytes],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async updateStakesOfOperatorSubsetForAllQuorums(
         operators: string[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'updateOperators',
-            [operators]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'updateOperators',
+            params: [operators],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async updateSocket(socket: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'updateSocket',
-            [socket]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'updateSocket',
+            params: [socket],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setRewardsInitiator(rewardsInitiatorAddr: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.serviceManager,
-            'setRewardsInitiator',
-            [rewardsInitiatorAddr]
-        );
+        return await sendContractCall({
+            contract: this.serviceManager,
+            method: 'setRewardsInitiator',
+            params: [rewardsInitiatorAddr],
+            abi: ABIs.SERVICE_MANAGER_BASE_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setSlashableStakeLookahead(
         quorumNumber: number,
         lookAheadPeriod: number
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.stakeRegistry,
-            'setSlashableStakeLookahead',
-            [quorumNumber, lookAheadPeriod]
-        );
+        return await sendContractCall({
+            contract: this.stakeRegistry,
+            method: 'setSlashableStakeLookahead',
+            params: [quorumNumber, lookAheadPeriod],
+            abi: ABIs.STAKE_REGISTRY_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setMinimumStakeForQuorum(
         quorumNumber: number,
         minimumStake: number
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.stakeRegistry,
-            'setMinimumStakeForQuorum',
-            [quorumNumber, minimumStake]
-        );
+        return await sendContractCall({
+            contract: this.stakeRegistry,
+            method: 'setMinimumStakeForQuorum',
+            params: [quorumNumber, minimumStake],
+            abi: ABIs.STAKE_REGISTRY_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async createTotalDelegatedStakeQuorum(
@@ -126,11 +149,14 @@ export class AvsRegistryWriter {
         minimumStakeRequired: Uint96,
         strategyParams: StrategyParams[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'createTotalDelegatedStakeQuorum',
-            [operatorSetParams, minimumStakeRequired, strategyParams]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'createTotalDelegatedStakeQuorum',
+            params: [operatorSetParams, minimumStakeRequired, strategyParams],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async createSlashableStakeQuorum(
@@ -139,11 +165,14 @@ export class AvsRegistryWriter {
         strategyParams: StrategyParams[],
         lookAheadPeriod: Uint32
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'createSlashableStakeQuorum',
-            [operatorSetParams, minimumStakeRequired, strategyParams, lookAheadPeriod]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'createSlashableStakeQuorum',
+            params: [operatorSetParams, minimumStakeRequired, strategyParams, lookAheadPeriod],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async ejectOperator(
@@ -151,38 +180,50 @@ export class AvsRegistryWriter {
         quorumNumbers: QuorumNum[]
     ): Promise<TransactionReceipt> {
         const quorumBytes = chainIoUtils.numsToBytes(quorumNumbers.map(Number));
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'ejectOperator',
-            [operatorAddress, quorumBytes]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'ejectOperator',
+            params: [operatorAddress, quorumBytes],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setOperatorSetParams(
         quorumNumber: number,
         operatorSetParams: OperatorSetParams
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'setOperatorSetParams',
-            [quorumNumber, operatorSetParams]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'setOperatorSetParams',
+            params: [quorumNumber, operatorSetParams],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setChurnApprover(churnApproverAddress: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'setChurnApprover',
-            [churnApproverAddress]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'setChurnApprover',
+            params: [churnApproverAddress],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setEjector(ejectorAddress: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'setEjector',
-            [ejectorAddress]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'setEjector',
+            params: [ejectorAddress],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async modifyStrategyParams(
@@ -190,76 +231,100 @@ export class AvsRegistryWriter {
         strategyIndices: number[],
         multipliers: number[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.stakeRegistry,
-            'modifyStrategyParams',
-            [quorumNumber, strategyIndices, multipliers]
-        );
+        return await sendContractCall({
+            contract: this.stakeRegistry,
+            method: 'modifyStrategyParams',
+            params: [quorumNumber, strategyIndices, multipliers],
+            abi: ABIs.STAKE_REGISTRY_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setAvs(avsAddress: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'setAVS',
-            [avsAddress]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'setAVS',
+            params: [avsAddress],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async setEjectionCooldown(ejectionCooldown: number): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.registryCoordinator,
-            'setEjectionCooldown',
-            [ejectionCooldown]
-        );
+        return await sendContractCall({
+            contract: this.registryCoordinator,
+            method: 'setEjectionCooldown',
+            params: [ejectionCooldown],
+            abi: ABIs.REGISTRY_COORDINATOR_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async addStrategies(
         quorumNumber: number,
         strategyParams: StrategyParams[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.stakeRegistry,
-            'addStrategies',
-            [quorumNumber, strategyParams]
-        );
+        return await sendContractCall({
+            contract: this.stakeRegistry,
+            method: 'addStrategies',
+            params: [quorumNumber, strategyParams],
+            abi: ABIs.STAKE_REGISTRY_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async updateAvsMetadataUri(metadataUri: string): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.serviceManager,
-            'updateAVSMetadataURI',
-            [metadataUri]
-        );
+        return await sendContractCall({
+            contract: this.serviceManager,
+            method: 'updateAVSMetadataURI',
+            params: [metadataUri],
+            abi: ABIs.SERVICE_MANAGER_BASE_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async removeStrategies(
         quorumNumber: number,
         indicesToRemove: number[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.stakeRegistry,
-            'removeStrategies',
-            [quorumNumber, indicesToRemove]
-        );
+        return await sendContractCall({
+            contract: this.stakeRegistry,
+            method: 'removeStrategies',
+            params: [quorumNumber, indicesToRemove],
+            abi: ABIs.STAKE_REGISTRY_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async createAvsRewardsSubmission(
         rewardsSubmission: RewardsSubmission[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.serviceManager,
-            'createAVSRewardsSubmission',
-            [rewardsSubmission]
-        );
+        return await sendContractCall({
+            contract: this.serviceManager,
+            method: 'createAVSRewardsSubmission',
+            params: [rewardsSubmission],
+            abi: ABIs.SERVICE_MANAGER_BASE_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 
     async createOperatorDirectedAvsRewardsSubmission(
-        operatorDirectedRewardsSubmissions: OperatorDirectedRewardsSubmissions[]
+        operatorDirectedRewardsSubmission: OperatorDirectedRewardsSubmission[]
     ): Promise<TransactionReceipt> {
-        return await this.sendTransaction(
-            this.serviceManager,
-            'createOperatorDirectedAVSRewardsSubmission',
-            [operatorDirectedRewardsSubmissions]
-        );
+        return await sendContractCall({
+            contract: this.serviceManager,
+            method: 'createOperatorDirectedAVSRewardsSubmission',
+            params: [operatorDirectedRewardsSubmission],
+            abi: ABIs.SERVICE_MANAGER_BASE_ABI,
+            pkWallet: this.pkWallet,
+            web3: this.ethHttpClient
+        });
     }
 }
